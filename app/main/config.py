e2cad38bd7f38c56156f97e79f92432189a2be95
Alex Maramaldo <alex@osbox.com.br>
2020-05-10 13:56:32 -0300

    adding port to setup

--- app/main/config.py
+++ app/main/config.py
@@ -9,7 +9,7 @@ try:
 
     env_path = Path('.') / 'docker/.env'
     load_dotenv(env_path)
-except Exception as ex: # pylint: disable=broad-except
+except Exception as ex:  # pylint: disable=broad-except
     pass
 
 
@@ -24,10 +24,11 @@ class DevelopmentConfig(Config):
     """ All configs to DevelopmentConfig
     """
     DEBUG = True
-    DB_URL = 'postgresql+psycopg2://{user}:{pw}@{host}/{db}'.format(
+    DB_URL = 'postgresql+psycopg2://{user}:{pw}@{host}:{post}/{db}'.format(
         user=os.getenv('DB_USERNAME'),
         pw=os.getenv('DB_PASSWORD'),
         host=os.getenv('DB_HOST'),
+        port=os.getenv('DB_PORT'),
         db=os.getenv('DB_DATABASE'))
     SQLALCHEMY_DATABASE_URI = DB_URL
     SQLALCHEMY_TRACK_MODIFICATIONS = False
@@ -38,7 +39,8 @@ class TestingConfig(Config):
     """
     DEBUG = True
     TESTING = True
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'flask_boilerplate_test.db')
+    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + \
+        os.path.join(basedir, 'flask_boilerplate_test.db')
     PRESERVE_CONTEXT_ON_EXCEPTION = False
     SQLALCHEMY_TRACK_MODIFICATIONS = False
 
