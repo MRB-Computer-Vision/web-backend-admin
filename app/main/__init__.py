3d5ff8075b4b02074a4cb1455c9b5271cd445ae9
Alex Maramaldo <alex@osbox.com.br>
2020-05-14 10:53:38 -0300

    adding cors

--- app/main/__init__.py
+++ app/main/__init__.py
@@ -16,5 +16,6 @@ def create_app(config_name):
     app.config.from_object(config_by_name[config_name])
     db.init_app(app)
     flask_bcrypt.init_app(app)
+    cors = CORS(app, resources={r"/*": {"origins": "*"}})
 
     return app
