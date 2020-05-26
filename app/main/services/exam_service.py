3bd8997a9f255d2603901aadd256b4a44aeec515
hashimoto <hashimotorafa@gmail.com>
2020-05-26 03:29:22 -0300

    predict image

--- app/main/services/exam_service.py
+++ app/main/services/exam_service.py
@@ -2,7 +2,10 @@
 from app.main import db
 from app.main.models.exam import Exam, ExamFile
 from app.main.repositories.exam_repository import ExamRepository
-import json
+from covid_vision.ml.models.covid_cxr import CovidCXR
+import os
+import boto3
+from urllib.parse import unquote
 
 def add_exam(data):
     # pylint: disable=no-member
@@ -10,9 +13,13 @@ def add_exam(data):
     try:
         exam_repository = ExamRepository()
         exam = exam_repository.save(data)
+        # run prediction
+        result = run_covid_predict(exam)
+        exam_repository.update_exam_result(result)
         response_object = {
             'success': True,
-            'message': 'Exam added with successfully.'
+            'message': 'Exam added with successfully.',
+            'data': exam.to_json()
         }
         return response_object, 201
     except Exception as e:
@@ -32,3 +39,17 @@ def get_a_exam(_id):  # pylint: disable=redefined-builtin
     """ Get User by ID
     """
     return Exam.query.filter_by(id=_id).first()
+
+def run_covid_predict(exam):
+    file_path = os.path.abspath(os.getcwd()) + '/app/main/tmp/'
+    if not os.path.exists(file_path): os.makedirs(file_path)
+    clf = CovidCXR()
+    s3 = boto3.client('s3', aws_access_key_id = os.getenv('S3_ACCESS_KEY'), aws_secret_access_key = os.getenv('S3_ACCESS_SECRET'))
+    # TODO improve the prediction result loop
+    for exam_file in exam.exam_files:
+        file_name = unquote(exam_file.file_path.partition('.com/')[-1])
+        s3.download_file(os.getenv('S3_BUCKET_NAME'), file_name, file_path + 'test.png')
+        img = clf.read_image(file_path + 'test.png')
+        result = clf.predict(img)
+        prediction = clf.CLASSES[result.argmax()]
+    return prediction
