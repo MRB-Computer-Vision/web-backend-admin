22e43cb0d11aa53bc01d7b49e95409ea146c3e8c
hashimoto <hashimotorafa@gmail.com>
2020-06-16 23:09:18 -0300

    create medical record dto

--- app/main/util/dto.py
+++ app/main/util/dto.py
@@ -46,3 +46,31 @@ class ExamDto:
         'updated_at': fields.String(required=False, description='updated_at'),
         'exam_files': fields.List(fields.Nested(exam_file), required=False)
     })
+
+class MedicalRecordDto:
+    """ ExamDTO Class
+    """
+    api = Namespace('medical_record', description='exam one related operations')
+
+    exam_file = api.model('exam_files', {
+        'id': fields.Integer(required=False, description='exam file id'),
+        'file_path': fields.String(required=True, description='type')
+    })
+
+    exam = api.model('exams', {
+        'id': fields.String(required=False, description='exam id'),
+        'status': fields.String(required=False, description='status'),
+        'result': fields.String(required=False, description='pct of covid19'),
+        'type': fields.String(required=False, description='type'),
+        'created_at': fields.String(required=False, description='created_at'),
+        'updated_at': fields.String(required=False, description='updated_at'),
+        'exam_files': fields.List(fields.Nested(exam_file), required=False)
+    })
+
+    medical_record = api.model('medical_records', {
+        'id': fields.String(required=False, description='medical_record id'),
+        'number': fields.String(required=False, description='status'),
+        'exams': fields.List(fields.Nested(exam), required=False),
+        'created_at': fields.String(required=False, description='created_at'),
+        'updated_at': fields.String(required=False, description='updated_at')
+    })
