827a25fe1c6cfbb3693200aa6b0daf9ce207f41e
Alex Maramaldo <alex@osbox.com.br>
2020-06-30 21:41:20 -0300

    fixing sitaxe

--- app/main/config.py
+++ app/main/config.py
@@ -43,32 +43,32 @@ class TestingConfig(Config):
         user=os.getenv('DB_USERNAME'),
         pw=os.getenv('DB_PASSWORD'),
         host=os.getenv('DB_HOST'),
-        port=evalos.getenv('DB_PORT')),
-        db = os.getenv('DB_DATABASE_TEST'))
-    SQLALCHEMY_DATABASE_URI=DB_URL
-    PRESERVE_CONTEXT_ON_EXCEPTION=False
+        port=eval(os.getenv('DB_PORT')),
+        db=os.getenv('DB_DATABASE_TEST'))
+    SQLALCHEMY_DATABASE_URI = DB_URL
+    PRESERVE_CONTEXT_ON_EXCEPTION = False
 
 
 class ProductionConfig(Config):
     """ All configs to ProductionConfig
     """
-    DEBUG=False
+    DEBUG = False
 
-    DB_URL='postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(
-        user = os.getenv('DB_USERNAME'),
-        pw = os.getenv('DB_PASSWORD'),
-        host = os.getenv('DB_HOST'),
-        port = eval(os.getenv('DB_PORT')),
-        db = os.getenv('DB_DATABASE'))
-    SQLALCHEMY_DATABASE_URI=DB_URL
+    DB_URL = 'postgresql+psycopg2://{user}:{pw}@{host}:{port}/{db}'.format(
+        user=os.getenv('DB_USERNAME'),
+        pw=os.getenv('DB_PASSWORD'),
+        host=os.getenv('DB_HOST'),
+        port=eval(os.getenv('DB_PORT')),
+        db=os.getenv('DB_DATABASE'))
+    SQLALCHEMY_DATABASE_URI = DB_URL
 
 
-config_by_name=dict(
-    dev = DevelopmentConfig,
-    test = TestingConfig,
-    prod = ProductionConfig
+config_by_name = dict(
+    dev=DevelopmentConfig,
+    test=TestingConfig,
+    prod=ProductionConfig
 
 
 )
 
-key=Config.SECRET_KEY
+key = Config.SECRET_KEY
